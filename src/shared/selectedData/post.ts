67d6c60a83512bfae8ab5a46b8f325c0dd0d970e
Matvey <matvejaksentev80@gmail.com>
2024-10-14 20:33:49 +0700

    Func likes

--- src/shared/selectedData/post.ts
+++ src/shared/selectedData/post.ts
@@ -3,7 +3,12 @@ import { DefaultArgs } from '@prisma/client/runtime/library';
 
 export const postSelectedData: Prisma.PostSelect<DefaultArgs> = {
   uuid: true,
-  likes: true,
+  likes: {
+    include: {
+      post: true,
+      user: true,
+    },
+  },
   content: true,
   hidden: true,
   shares: true,
